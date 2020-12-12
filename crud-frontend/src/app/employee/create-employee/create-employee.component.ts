a4cc8f4e835c9874c284074072d52cc86b6f65be
Renan Roseno <rosenorenan@gmail.com>
2020-12-12 17:21:38 -0300

    Started to styling create employee view

--- crud-frontend/src/app/employee/create-employee/create-employee.component.ts
+++ crud-frontend/src/app/employee/create-employee/create-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-create-employee',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-
+  employee: Employee = new Employee();
+  
   constructor() { }
 
   ngOnInit(): void {
