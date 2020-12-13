8f717665cb4969b5d681fde078f3eb6a6845054c
Renan Roseno <rosenorenan@gmail.com>
2020-12-13 16:32:00 -0300

    Delete function implemented

--- crud-backend/src/main/java/com/crud/backend/controllers/EmployeeController.java
+++ crud-backend/src/main/java/com/crud/backend/controllers/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.crud.backend.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,5 +60,18 @@ public class EmployeeController {
 		Employee updatedEmployee = employeeRepository.save(employee);
 
 		return ResponseEntity.ok(updatedEmployee);
+	}
+
+	@DeleteMapping("/funcionarios/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Integer id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Funcionário não existe com o id" + id));
+
+		employeeRepository.delete(employee);
+
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("Excluido", Boolean.TRUE);
+
+		return ResponseEntity.ok(response);
 	}
 }
