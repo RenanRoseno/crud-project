03309e2e8aa1e7cc60d99bcc9073a03d5664a296
Renan Roseno <rosenorenan@gmail.com>
2020-12-13 16:56:30 -0300

    Delete component created and cofigurated

--- crud-frontend/src/app/employee/employee-list/employee-list.component.ts
+++ crud-frontend/src/app/employee/employee-list/employee-list.component.ts
@@ -31,4 +31,11 @@ export class EmployeeListComponent implements OnInit {
   updateEmployee(id : number) {
     this.router.navigate(["funcionarios/editar/", id])
   }
+
+  deleteEmployee(id : number){
+    this.employeeService.deleteEmployee(id).subscribe(data => {
+      console.log(data);
+      this.getEmployees();
+    })
+  }
 }
